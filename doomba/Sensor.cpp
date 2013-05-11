947794a9b9870725585be7ee0d3d60c1167387b4
Michael Moskie <michael.moskie@gmail.com>
2013-05-10 20:57:28 -0400

    Added sensor things

--- doomba/Sensor.cpp
+++ doomba/Sensor.cpp
@@ -1,6 +1,10 @@
 #include "Sensor.h"
 #include "Servo.h"
 Servo sensorServo;
+Sensor::Sensor()
+{
+	Serial.begin(9600);
+}
 int Sensor::getDistance(int degree)
 {
 	sensorServo.attach(3);
--- doomba/Sensor.h
+++ doomba/Sensor.h
@@ -6,6 +6,7 @@
 
 class Sensor{
 public:
+	Sensor();
     int getDistance(int degree);
 };
 
